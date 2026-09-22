0c6d2ab704f9337648ff62f6e0d78075746acaad
shekhussainbasha1103-lgtm <shekhussainbasha1103@gmail.com>
2026-09-23 01:42:44 +0530

    Create sum_of_digits.c

---
+++ sum_of_digits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+        n = -n;
+
+    while (n > 0) {
+        digit = n % 10;
+        sum = sum + digit;
+        n = n / 10;
+    }
+
+    printf("Sum of digits = %d", sum);
+
+    return 0;
+}
